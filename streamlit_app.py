802e2c9e741430d7c85d9d62c7f7d16a3f80e962
sharazar1 <126076215+sharazar1@users.noreply.github.com>
2024-12-12 06:13:40 +0000

    12/12/2024

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,7 @@ import streamlit as st
 
 st.title("This is a Title")
 st.header("This is a Header")
+st.checkbox(label="This is a checkbox", key="T&C accepted", help="Check to accept T&C")
 
-st.image("bg.jpg")
+radio_btn= st.radio(label = "In which country do you live?", options = ("US & Canada", "UK", "Europe", "Middle East"))
+print(radio_btn)
