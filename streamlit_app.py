6eab40a67da364a91b1589ea2556c3f78f154fd7
sharazar1 <126076215+sharazar1@users.noreply.github.com>
2024-12-12 19:22:11 +0000

    text inputs

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,3 +17,16 @@ print(select)
 
 multi_select = st.multiselect("What is your facourite Thing to drink?", options=("Cofee", "Tea", "Water", "Cold Drinks"))
 st.write(multi_select)
+
+allowed_img_ext = ["png", "jpg", "jpeg", "heic"]
+
+uploaded_image = st.file_uploader("Upload an Image please", type=allowed_img_ext, accept_multiple_files= True)
+if uploaded_image is not None:
+    st.image(uploaded_image)
+
+st.slider(label="This is a slider", min_value=1, max_value=10)
+
+text_small = st.text_input(label="Enter ssome text", max_chars=10)
+#text_big = st.text_area(label="Enter big text")
+st.text(text_small)
+print(text_small)
