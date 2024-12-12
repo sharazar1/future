78b4b22f4d284c936ae3cff645def57d9f6646bd
sharazar1 <126076215+sharazar1@users.noreply.github.com>
2024-12-12 06:34:13 +0000

    show to H

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,14 @@ st.checkbox(label="This is a checkbox", key="T&C accepted", help="Check to accep
 
 radio_btn= st.radio(label = "In which country do you live?", options = ("US & Canada", "UK", "Europe", "Middle East"))
 print(radio_btn)
+def clicked():
+    print("Button clicked")
+
+btn = st.button("Click me", on_click = clicked)
+
+select=st.selectbox("What is your favourite car", options=("BMW", "AUDI", "Ferrari"))
+
+print(select)
+
+multi_select = st.multiselect("What is your facourite Thing to drink?", options=("Cofee", "Tea", "Water", "Cold Drinks"))
+st.write(multi_select)
