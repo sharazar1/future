d1af10e56bb0a6fb32b5636d513ad24265139fa6
sharazar1 <126076215+sharazar1@users.noreply.github.com>
2024-12-13 06:26:41 +0000

    13/12/2024

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-
+import time
+# from datetime import time
 st.title("This is a Title")
 st.header("This is a Header")
 st.checkbox(label="This is a checkbox", key="T&C accepted", help="Check to accept T&C")
@@ -30,3 +31,10 @@ text_small = st.text_input(label="Enter ssome text", max_chars=10)
 #text_big = st.text_area(label="Enter big text")
 st.text(text_small)
 print(text_small)
+dt = st.date_input("Enter your date")
+tm = st.time_input("Enter Time")
+# Progress Bars
+bar = st.progress(50)
+for i in range(10):
+    bar.progress((i+1)*10)
+    time.sleep(1)
