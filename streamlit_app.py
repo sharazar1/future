3dd25cb2052915770c8c5ac65b99bbc5387d9b92
sharazar1 <126076215+sharazar1@users.noreply.github.com>
2024-12-16 16:47:49 +0000

    Forms

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,32 @@ bar = st.progress(50)
 for i in range(10):
     bar.progress((i+1)*10)
     time.sleep(1)
+
+st.markdown("<h1 style ='text-align: center;'>Contact Me</h1>", unsafe_allow_html=True)
+# 2 methods for forms
+contact_form = st.form("Contact")
+
+contact_form.text_input("Your Name")
+# Always create submit button
+contact_form._form_submit_button("Submit Form")
+
+
+
+# 2nd method
+with st.form("Form 2", clear_on_submit=True):
+    col1, col2 = st.columns(2)
+    f_name= col1.text_input("First Name")
+    l_name = col2.text_input("Last Name")
+    day,month,year = st.columns(3)
+    day.text_input("Day")
+    month.text_input("Month")
+    year.text_input("Year")
+    st.text_input("Email Adress!")
+    st.text_input("Password")
+    st.text_input("input passpwrd again")
+    s_state = st.form_submit_button("Submit")
+    if s_state:
+        if f_name == "" and l_name =="":
+            st.warning("Please check all fields")
+        else:
+            st.success("Submitted Successfully!")
