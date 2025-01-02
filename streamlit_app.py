e1d987e76d4a1fc25d13e5c83a385c03a792bea0
sharazar1 <126076215+sharazar1@users.noreply.github.com>
2025-01-02 05:29:17 +0000

    file name changed

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello & Welcome!",
+    page_icon="👋",
+)
+
+st.title("Welcome Visitor!! 👋")
+st.header("Sharazar Munawar")
+st.subheader("Bridging the Gap Between Data and Infrastructure")
